5701710571728f30c3660d71c4504fb7524d6cd8
agonza26 <agonza26@ucsc.edu>
2015-04-08 13:57:02 -0700

    Asg 2: maybe finished first bot

--- Assignment 2/README.txt
+++ Assignment 2/README.txt
@@ -2,4 +2,8 @@ Assignment 2
 
 Alexander Gonzalez agonza26
 
-usage: python [filename] [ ] [ ] 
+usage:  python p2_sim.py
+
+		or
+
+	python p2_gui.py 
--- Assignment 2/first_bot.py
+++ Assignment 2/first_bot.py
@@ -1,2 +1,5 @@
+import random
+import math
 def think(state, quip):
-  return state.get_moves()[0]
+   
+  return state.get_moves()[math.floor(random.random())*  len( state.get_moves())]
