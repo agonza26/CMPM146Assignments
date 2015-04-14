997726400c96af1fd6dc1a067be06c2b066e75cd
agonza26 <agonza26@ucsc.edu>
2015-04-14 08:34:51 -0700

    changed greedy and added doc

--- Assignment 2/greedy_bot.py
+++ Assignment 2/greedy_bot.py
@@ -2,15 +2,17 @@ from random import randint
 
 def think(state, quip):
 	bestMove = None
+	bestScore = state.get_score()[state.get_whos_turn()] 
 
 	#print state.get_score()[state.get_whos_turn()]
 	for i in state.get_moves():
 		stateCopy = state.copy()
 		stateCopy.apply_move(i)
 
-		if state.get_score()[state.get_whos_turn()] < stateCopy.get_score()[state.get_whos_turn()]:
+		if bestScore < stateCopy.get_score()[state.get_whos_turn()]:
+			bestScore = stateCopy.get_score()[state.get_whos_turn()]
 			bestMove = i 
-			break
+			
 	if bestMove == None:
 		bestMove=state.get_moves()[  randint(0,len( state.get_moves())-1)  ]
 
--- Assignment 2/uniform_bot.py
+++ Assignment 2/uniform_bot.py
@@ -1,5 +1,5 @@
 import random
 import math
 def think(state, quip):
-  
-  return state.get_moves()[  (int)(math.floor(random.random() *  len( state.get_moves())))   ]
+  #random.choice
+  return state.get_moves()[  (int)(math.floor(random.random() *  len( state.get_moves())))]
